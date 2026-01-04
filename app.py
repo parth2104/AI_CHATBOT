2db7ae0f49f8b918ee0403ea5ce21a7771893668
parth2104 <barotparth512@gmail.com>
2026-01-04 16:45:12 +0530

    application building

--- app.py
+++ app.py
@@ -1,23 +1,45 @@
 import streamlit as st
+from langchain_groq import ChatGroq
+from langchain_core.output_parsers import StrOutputParser
+from prompt.prompt_template import chat
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
-st.title("RAG Chatbot!")
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+st.title("RAG APPLICATION")
 
 if "messages" not in st.session_state:
     st.session_state.messages=[]
 
 for message in st.session_state.messages:
-    st.chat_message(message['role']).markdown(message['content'])
+    st.chat_message(message["role"]).markdown(message["content"])
 
+prompt=st.chat_input("write your query")
 
+if prompt:
 
-promtp=st.chat_input("write your query")
+    st.chat_message("user").markdown(prompt)
+    st.session_state.messages.append({"role":"user","content":prompt})
 
-if promtp:
-    st.chat_message("user").markdown(promtp)
+    @st.cache_resource
+    def get_vectors():
+        jd=
 
-    st.session_state.messages.append({"role":"user","content":promtp})
-    respose='I am your Assistant'
-    st.chat_message("Assistant").markdown(respose)
+    chat_model=ChatGroq(
+        model="llama-3.1-8b-instant",
+        api_key=os.environ.get("groq_api")
+    )
 
-    st.session_state.messages.append({"role":"user","content":respose})
+    chain= chat | chat_model |StrOutputParser()
+    response=chain.invoke({"question":prompt})
+    
+    
+    st.chat_message("assistant").markdown(response)
+    st.session_state.messages.append({"role":"assistant","content":response})
+
