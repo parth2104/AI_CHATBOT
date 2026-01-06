4d8be8fbad2468099e83ba07bdf2078fe41287af
parth2104 <barotparth512@gmail.com>
2026-01-06 15:26:35 +0530

    application

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from prompt.prompt_template import chat
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
 
 
 import os
@@ -20,6 +21,20 @@ if "messages" not in st.session_state:
 for message in st.session_state.messages:
     st.chat_message(message["role"]).markdown(message["content"])
 
+st.cache_resource
+def get_vectorstores():
+        pdf_path=r"documents\NIPS-2017-attention-is-all-you-need-Paper.pdf"
+        loader=PyPDFLoader(pdf_path)
+        document_loader=loader.load()
+        splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=100)
+        chunks=splitter.split_documents(document_loader)
+        embedding=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2cls")
+
+        vectostore=Chroma.from_documents(chunks,embedding)
+
+        return vectostore
+vector=get_vectorstores()
+
 prompt=st.chat_input("write your query")
 
 if prompt:
@@ -27,9 +42,8 @@ if prompt:
     st.chat_message("user").markdown(prompt)
     st.session_state.messages.append({"role":"user","content":prompt})
 
-    @st.cache_resource
-    def get_vectors():
-        jd=
+    doc=vector.similarity_search(prompt,k=3)
+
 
     chat_model=ChatGroq(
         model="llama-3.1-8b-instant",
